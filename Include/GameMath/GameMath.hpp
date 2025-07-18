cdb356358f446f5ec1c00c885f29b3917c4ffdd7
bt-austin <tavaresaustinbarton@gmail.com>
2025-07-17 22:05:50 -0700

    Added New Source: GameMath.hpp

---
+++ Include/GameMath/GameMath.hpp
@@ -0,0 +1,10 @@
+#ifndef GAME_MATH_HPP
+#define GAME_MATH_HPP
+
+//==========================================================================//
+// INCLUDES																	//
+//==========================================================================//
+
+#include <GameMath/Vectors/Vector2.hpp>
+
+#endif // !GAME_MATH_HPP
